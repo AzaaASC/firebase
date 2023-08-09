069b4a67510a57bd660bda57b5b6331e18399668
AzaaASC <140005763+AzaaASC@users.noreply.github.com>
2023-08-09 15:41:17 -0400

    Add files via upload

--- init-firebase.js
+++ init-firebase.js
@@ -3,8 +3,13 @@
  * provided for you by firebase after creating your project!
 */
 const firebaseConfig = {
-    
-};
-
+    apiKey: "AIzaSyBjsq0plO7HbmKitvRtniYtarvJk3SWC4M",
+    authDomain: "firemessages-6a251.firebaseapp.com",
+    projectId: "firemessages-6a251",
+    storageBucket: "firemessages-6a251.appspot.com",
+    messagingSenderId: "687406055735",
+    appId: "1:687406055735:web:f636a9a9a9cc2fefba5ab2"
+  };
+  
 // initialize the firebase app with this built-in function
 firebase.initializeApp(firebaseConfig);
--- main.js
+++ main.js
@@ -1,66 +1,60 @@
-/**
- * @TODO get a reference to the Firebase Database object
- */
+// main.js
 
-/**
- * @TODO get const references to the following elements:
- *      - div with id #all-messages
- *      - input with id #username
- *      - input with id #message
- *      - button with id #send-btn and the updateDB
- *        function as an onclick event handler
- */
+// Get a reference to the Firebase Database object
+const database = firebase.database().ref();
 
-/**
- * @TODO create a function called updateDB which takes
- * one parameter, the event, that:
- *      - gets the values of the input elements and stores
- *        the data in a temporary object with the keys USERNAME
- *        and MESSAGE
- *      - console.logs the object above
- *      - writes this object to the database
- *      - resets the value of #message input element
- */
+// Get const references to the elements
+const allMessages = document.getElementById('all-messages');
+const usernameElem = document.getElementById('username');
+const messageElem = document.getElementById('message');
+const sendBtn = document.getElementById('send-btn');
 
-/**
- * @TODO add the addMessageToBoard function as an event
- * handler for the "child_added" event on the database
- * object
- */
+// Use lowercase 'click' for the event name and assign the updateDB function
+sendBtn.addEventListener('click', updateDB);
 
-/**
- * @TODO create a function called addMessageToBoard that
- * takes one parameter rowData which:
- *      - console.logs the data within rowData
- *      - creates a new HTML element for a single message
- *        containing the appropriate data
- *      - appends this HTML to the div with id
- *        #all-messages (we should have a reference already!)
- * 
- */
+// Function to update the database
+function updateDB(event) {
+    event.preventDefault();
 
-/** 
- * @TODO create a function called makeSingleMessageHTML which takes
- * two parameters, usernameTxt and messageTxt, that:
- *      - creates a parent div with the class .single-message
- * 
- *      - creates a p tag with the class .single-message-username
- *      - update the innerHTML of this p to be the username 
- *        provided in the parameter object
- *      - appends this p tag to the parent div
- * 
- *      - creates a p tag
- *      - updates the innerHTML of this p to be the message
- *        text provided in the parameter object
- *      - appends this p tag to the parent div
- * 
- *      - returns the parent div
- */
+    const username = usernameElem.value;
+    const message = messageElem.value;
+    usernameElem.value = '';
+    messageElem.value = '';
 
-/**
- * @BONUS add an onkeyup event handler to the form HTML
- * element so the user can also submit the form with the
- * Enter key
- * 
- * @BONUS use an arrow function
- */
+    console.log(username + ' : ' + message);
+
+    const value = {
+        USERNAME: username,
+        MESSAGE: message
+    };
+
+    database.push(value);
+}
+
+// Function to add a message to the board
+function addMessageToBoard(rowData) {
+    const data = rowData.val();
+    let singleMessageElem = makeSingleMessageHTML(data.USERNAME, data.MESSAGE);
+    allMessages.appendChild(singleMessageElem);
+}
+
+// Function to create a single message HTML element
+function makeSingleMessageHTML(usernameTxt, messageTxt) {
+    let parentDiv = document.createElement('div');
+    parentDiv.classList.add('single-message');
+
+    let usernameP = document.createElement('p');
+    usernameP.classList.add('single-message-username');
+    usernameP.innerHTML = usernameTxt;
+
+    let messageP = document.createElement('p');
+    messageP.innerHTML = messageTxt;
+
+    parentDiv.appendChild(usernameP);
+    parentDiv.appendChild(messageP);
+
+    return parentDiv;
+}
+
+// Set up the child_added event listener
+database.on('child_added', addMessageToBoard);
